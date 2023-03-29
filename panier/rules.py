f395ab9c44f01e4b7bd9321311d07fdf7ef5a708
aymankr <kr.ayman@hotmail.com>
2023-03-29 12:29:11 +0200

    generate, validation, cross

--- panier/rules.py
+++ panier/rules.py
@@ -17,16 +17,93 @@ class Arules:
         return self.support_itemsets.get(tuple(set(tg + td)))
 
     def confidence(self, tg : tuple, td : tuple) -> float:
-        return self.support(tg + td, ()) / self.support(tg, ())
+        return self.support(tg, td) / self.support(tg, ())
 
     def lift(self, tg : tuple, td : tuple) -> float:
         return self.support(tg, td) / \
             (self.support(tg, ())*self.support(td, ()))
 
     def leverage(self, tg : tuple, td : tuple) -> float:
-        # 
-        pass
+        return self.support(tg, td) - (self.support(tg, ()) * self.support((), td))
 
     def conviction(self, tg : tuple, td : tuple) -> float:
-        return (1 - self.support((), td)) / \
-            (1 - self.confidence(tg, td))
+        if (1 - self.confidence(tg, td)) != 0:
+            return (1 - self.support((), td)) / \
+                (1 - self.confidence(tg, td))
+        else:
+            return 0
+    
+    def lift_diag(self, tg : tuple, td : tuple):
+        value = self.lift(tg, td)
+        
+        if value == 1:
+            message = f"{tg} -> {td} ne pas utiliser"
+        elif value < 1:
+            message = f"{tg} et {td} ne peuvent pas co-exister dans une règle"
+        else:
+            message = f"{tg} -> {td} est prédictive"
+
+        return message
+        
+    def generate_rules(self, min_confidence):
+        self.rules = []
+
+        for itemset_list in self.list_itemsets[1:]:
+            for ref_itemset in itemset_list:
+                k = len(ref_itemset)
+                ref_items = [(item,) for item in ref_itemset]
+
+                if k == 2:
+                    self.rules += self.validation_rules(ref_itemset, ref_items, min_confidence)
+                else:
+                    self.rules += self.build_rules(ref_itemset, ref_items, min_confidence)
+
+        return self.rules
+
+    def build_rules(self, ref_itemset, rhs_p, min_confidence):
+        local_rules = []
+        k = len(ref_itemset)
+
+        if k > len(rhs_p[0]) + 1:
+            rhs_q = self.cross_product(rhs_p)
+            local_rules += self.validation_rules(ref_itemset, rhs_q, min_confidence)
+            local_rules += self.build_rules(ref_itemset, rhs_q, min_confidence)
+
+        return local_rules
+
+    def validation_rules(self, itemset, candidates, min_confidence):
+        valid_rules = []
+        accepted_rhs = []
+        for rhs in candidates:
+            lhs = tuple(sorted(set(itemset) - set(rhs)))
+            conf = self.confidence(lhs, rhs)
+            if conf >= min_confidence:
+                rule = (lhs, rhs)
+                valid_rules.append(rule)
+                self.rules.append(rule)
+                accepted_rhs.append(rhs)
+
+        return accepted_rhs
+
+    def cross_product(self, L):
+        rep = []
+        taille = len(L)
+
+        for i in range(taille - 1):
+            j = i + 1
+            while j < taille and L[i][:-1] == L[j][:-1]:
+                nouveau = L[i] + (L[j][-1],)
+
+                all_subsets_present = True
+                for x in range(len(nouveau)):
+                    sub_itemset = nouveau[:x] + nouveau[x + 1:]
+                    if sub_itemset not in L:
+                        all_subsets_present = False
+                        break
+
+                if all_subsets_present:
+                    rep.append(nouveau)
+
+                j = j + 1
+
+        return rep
--- panier/sortie_arules.py
+++ panier/sortie_arules.py
@@ -32,3 +32,28 @@ for lhs,rhs in [ [(2,5), (3,)], [(2,3), (5,)], [(3,5), (2,)] ]:
         _r = round(_r, 3) if isinstance(_r, float) else _r
         print("{} : {}".format(getattr(br, m).__name__, _r))
         print("#"+"="*73+"#")
+
+## generate
+print("{0} generate_rules {0}".format('*=*'))
+data = {100:[1, 3, 4], 200:[2, 3, 5], 300:[1, 2, 3, 5], 400:[2, 5]}
+db = Apriori(data)
+br = Arules(db.main(.5), db.support_history)
+for k in (.75, .5, .25, .1):
+    print("min confiance", k)
+    print("rules before generation", br.rules)
+    _out = br.generate_rules( k )
+    print("rules after generate_rules", br.rules)
+    print("return of generate_rules ?", _out)
+    print("*"*17)
+## validate 
+data = {100:[1, 3, 4], 200:[2, 3, 5], 300:[1, 2, 3, 5], 400:[2, 5]}
+db = Apriori(data)
+br = Arules(db.main(.5), db.support_history)
+print("rules before validation", br.rules)
+_candidates = [(2,), (3,), (5,), (2,3), (2,5), (3,5)]
+threshold = 3/4
+print("candidates {} and minConfidence is {:.2f}"
+"".format(_candidates, threshold))
+_out = br.validation_rules((2,3,5), _candidates, threshold)
+print("rules after validation", br.rules)
+print("accepted rhs", _out)
